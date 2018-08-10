6837ea94352988316397fac57b19d001457e4d1e
Boris Katsnelson <bmk98@yahoo.com>
2018-08-10 12:44:37 -0400

    Change deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,20 +8,62 @@ var ProposalsLibrary = artifacts.require("./ProposalsLibrary.sol");
 var SecurityLibrary = artifacts.require("./SecurityLibrary.sol");
 var TokenLedger = artifacts.require("./TokenLedger.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(DataVerifiable);
-  deployer.deploy(EternalStorage);
-  deployer.deploy(OrganisationUpdated);
-  deployer.deploy(Ownable);
-  deployer.deploy(TokenLedger);
+module.exports = function(deployer, network, accounts) {
 
-  deployer.deploy(ProposalsLibrary);
-  deployer.deploy(SecurityLibrary);
-  deployer.link(ProposalsLibrary, Organisation);
-  deployer.link(SecurityLibrary, Organisation);
-  deployer.deploy(Organisation);
+  deployer.then(() => { 
+  
+    return deployer.deploy(DataVerifiable)
+      .then(() => {
+        return deployer.deploy(Ownable)
+      })
+      .then(() => {
+        return deployer.deploy(EternalStorage)
+      })
+      .then(() => {
+        return deployer.deploy(TokenLedger)
+      })
+      .then(() => {
+        return deployer.deploy(OrganisationUpdated, TokenLedger.address, EternalStorage.address)
+      })
+      .then(() => {
+        return deployer.deploy(ProposalsLibrary)
+      })
+      .then(() => {
+        return deployer.deploy(SecurityLibrary)
+      })
+      .then(() => {
+        return deployer.link(ProposalsLibrary, Organisation)
+      })
+      .then(() => {
+        return deployer.link(SecurityLibrary, Organisation)
+      })
+      .then(() => {
+        return deployer.deploy(Organisation, TokenLedger.address, EternalStorage.address)
+      })
+      .then(() => {
+        return deployer.link(ProposalsLibrary, Parent)
+      })
+      .then(() => {
+        return deployer.link(SecurityLibrary, Parent)
+      })
+      .then(() => {
+        return deployer.deploy(Parent)
+      })
+  })
 
-  deployer.link(ProposalsLibrary, Parent);
-  deployer.link(SecurityLibrary, Parent);
-  deployer.deploy(Parent);
-};
+  // deployer.deploy(DataVerifiable);
+  // deployer.deploy(EternalStorage);
+  // deployer.deploy(OrganisationUpdated);
+  // deployer.deploy(Ownable);
+  // deployer.deploy(TokenLedger);
+
+  // deployer.deploy(ProposalsLibrary);
+  // deployer.deploy(SecurityLibrary);
+  // deployer.link(ProposalsLibrary, Organisation);
+  // deployer.link(SecurityLibrary, Organisation);
+  // deployer.deploy(Organisation);
+
+  // deployer.link(ProposalsLibrary, Parent);
+  // deployer.link(SecurityLibrary, Parent);
+  // deployer.deploy(Parent);
+}
